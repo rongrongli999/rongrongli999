ea257396b96089466cba0d3a541f16abbea6bb9a
rongrongli999 <ljr2007102@qq.com>
2025-10-25 00:24:20 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,8 +2,18 @@
 //
 //
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+int main() {
+  // 定义float类型变量存储摄氏温度和华氏温度
+    float celsius, fahrenheit;
+    
+    // 获取用户输入的摄氏温度
+    scanf("%f", &celsius);
+    
+    // 按公式计算华氏温度
+    fahrenheit = celsius * 9 / 5 + 32;
+    
+    // 输出结果，保留1位小数
+    printf("%.1f\n", fahrenheit);
+    
     return 0;
 }
